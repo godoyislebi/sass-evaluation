e35f1abfdd04169015044ace0edc3174f466dc34
godoyislebi <islebi.pinto.godoy24@gmail.com>
2016-12-24 00:47:42 -0300

    maquetación en proceso

---
+++ js/main.js
@@ -0,0 +1,30 @@
+ $('.dropdown-button').dropdown({  
+      inDuration: 300,
+      outDuration: 225,
+      constrain_width: false, // Does not change width of dropdown to that of the activator
+      hover: true, // Activate on hover
+      gutter: 0, // Spacing from edge
+      belowOrigin: false, // Displays dropdown below the button
+      alignment: 'left' // Displays dropdown with edge aligned to the left of button
+    }
+  );
+  $('.dropdown-button').dropdown('open');
+  $('.dropdown-button').dropdown('close');
+
+
+ 
+
+
+  $(document).ready(function(){  //slider
+      $('.slider').slider({full_width: true});
+    });
+
+  // Pause slider
+$('.slider').slider('pause');
+// Start slider
+$('.slider').slider('start');
+// Next slide
+$('.slider').slider('next');
+// Previous slide
+$('.slider').slider('prev');
+      
